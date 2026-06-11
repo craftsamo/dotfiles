ce232abe6279cd572bcf48d4da376b28e707c169
CraftSamo <craftsamo.g@gmail.com>
2026-06-11 20:10:06 +0800

    fix(zsh): default to emacs keymap so the Ctrl-F fzf binding works

--- zsh/functions/zsh_user_key_bindings.zsh
+++ zsh/functions/zsh_user_key_bindings.zsh
@@ -1,5 +1,8 @@
 source ~/.config/zsh/functions/fzf_change_directory.zsh
 
+# Use emacs keymap regardless of $EDITOR (nvim makes zsh default to viins)
+bindkey -e
+
 # peco
 zle -N fzf_change_directory
 bindkey -M emacs '^F' fzf_change_directory
